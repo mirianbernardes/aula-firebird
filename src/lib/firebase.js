7dd4de604c367218182aad00272861e47610fb39
(mirianbernardes) <(mirianbernardes@gmail.com)>
2022-08-02 21:43:14 -0300

    alteração das keys

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOm8hC6p5g8biDaXbzxTzMcPnCY_HRKCg",
-  authDomain: "miaulafirebase.firebaseapp.com",
-  projectId: "miaulafirebase",
-  storageBucket: "miaulafirebase.appspot.com",
-  messagingSenderId: "529401068551",
-  appId: "1:529401068551:web:e67cac243525fb7f673ea2"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTO_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SEND_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
